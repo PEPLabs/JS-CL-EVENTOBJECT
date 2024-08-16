693d49390b4281487203431c3a26b0398c47e33e
Dilip Babu <106616107+619DILIP@users.noreply.github.com>
2024-08-16 15:23:07 -0400

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -22,7 +22,7 @@ public class SeleniumTest {
         System.setProperty("webdriver.chrome.driver", "driver/chromedriver");//linux_64
 
         // Get file
-        File file = new File("EventObject.html");
+        File file = new File("src/main/EventObject.html");
         String path = "file://" + file.getAbsolutePath();
 
         // Create a new ChromeDriver instance
